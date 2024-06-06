d1814352ed233ef6daf1660edd86fbf3d15ea167
Raphael Fleury <raphael.fleury9@gmail.com>
2024-06-05 22:37:55 -0300

    feat: create dropdown menu component

---
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -0,0 +1,40 @@
+import { Component, ContentChild, ElementRef, Input, Renderer2, TemplateRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.css'
+})
+export class DropdownComponent {
+  @ViewChild('menu') container!: ElementRef;
+  @Input() controller!: HTMLElement
+  @Input() top: string | undefined
+  @Input() bottom: string | undefined 
+  @Input() left: string | undefined
+  @Input() right: string | undefined
+
+  hidden = false
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      const isTarget = this.container.nativeElement.contains(e.target)
+      const controllerIsTarget = (this.controller as any).contains(e.target)
+      console.log({isTarget, controllerIsTarget})
+      if (!isTarget && !controllerIsTarget) {
+        this.hidden = true;
+      }
+    });
+  }
+
+  get style() {
+    return `display: ${this.hidden ? 'none; ' : 'block; '}`
+    + (this.top ? `top: ${this.top}; ` : '')
+    + (this.bottom ? `bottom: ${this.bottom}; ` : '')
+    + (this.left ? `left: ${this.left}; ` : '')
+    + (this.right ? `right: ${this.right}; ` : '')
+  }
+
+  toggle() {
+    this.hidden = !this.hidden
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollNearEndDirective } from './directives/scroll-near-end.directive';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
-    ScrollNearEndDirective
+    ScrollNearEndDirective,
+    DropdownComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [ScrollNearEndDirective]
+  exports: [ScrollNearEndDirective, DropdownComponent]
 })
 export class SharedModule { }
